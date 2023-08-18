9278968f027a3646ae1f93e4c6ef249e216b4dac
Aadarsh Mane <onlyaddy68@gmail.com>
2023-08-18 11:12:16 +0530

    added cors package

--- routes/news.js
+++ routes/news.js
@@ -1,10 +1,11 @@
 import express from "express";
-
+import cors from "cors";
 import axios from "axios";
 import cheerio from "cheerio";
 import { allNews, getSpecificNews } from "../controllers/news.js";
 const router = express.Router();
-
+const app = express();
+app.use(cors());
 router.get("/", allNews);
 router.get("/:newspaperId", getSpecificNews);
 
